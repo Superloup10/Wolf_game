aeea529c7488456e93829038753fa801be4aafa7
Superloup10 <Superloup10@gmail.com>
2014-03-24 19:11:34 +0100

    change Render and Textures

--- src/fr/wolf/Wolf.java
+++ src/fr/wolf/Wolf.java
@@ -1,5 +1,20 @@
 package fr.wolf;
 
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.util.glu.GLU.gluOrtho2D;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
@@ -12,6 +27,8 @@ import fr.wolf.moteur.Keybinding;
 import fr.wolf.moteur.collision.entity.DummyEntity;
 import fr.wolf.moteur.collision.entity.EntityManager;
 import fr.wolf.moteur.collision.entity.Tile;
+import fr.wolf.moteur.graphic.Sprite;
+import fr.wolf.moteur.graphic.SpriteSheet;
 import fr.wolf.world.World;
 
 public class Wolf
@@ -19,6 +36,9 @@ public class Wolf
 	int fps;
 	long lastFps;	
 	private long lastFrame;
+	
+	public static int WIDTH = 1280;
+	public static int HEIGHT = 720;
 	
 	public static World world = new World();
 
@@ -32,17 +52,26 @@ public class Wolf
 	{
 		try
 		{
-			Display.setDisplayMode(new DisplayMode(1280, 720));
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
 			Display.create();
 			Display.setTitle("Wolf");
 			Display.setResizable(true);
+			SpriteSheet.init();
+			Sprite.init();
+			
+			//Init Transparence
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			
+			//Init Texture
+			glEnable(GL_TEXTURE_2D);
 		} catch (LWJGLException e)
 		{
 			e.printStackTrace();
 			System.exit(1);
 		}
 		// init OpenGL here
-		initGL();
+		//initGL();
 
 		entityManager.registerEntity(player);
 		entityManager.registerEntity(dummy);
@@ -55,6 +84,8 @@ public class Wolf
 			Keybinding.input();
 			render();
 			update();
+			WIDTH = Display.getWidth();
+			HEIGHT = Display.getHeight();
 			Display.update();
 			Display.sync(60);
 		}
@@ -76,25 +107,40 @@ public class Wolf
 
 	public void initGL()
 	{
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		glViewport(0, 0, Display.getWidth(), Display.getHeight());
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluOrtho2D(0, WIDTH, HEIGHT, 0);
+		glMatrixMode(GL_MODELVIEW);
+		/*GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glShadeModel(GL11.GL_SMOOTH);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		GL11.glClearDepth(1);
-		GL11.glViewport(0, 0, 1280, 720);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 1280, 720, 0, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glOrtho(0, WIDTH, HEIGHT, 0, 1, -1);*/
 	}
 
 	public void render()
 	{
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		entityManager.render();
+		glClear(GL_COLOR_BUFFER_BIT);
+		initGL();
+		//entityManager.render();
+		
+		GL11.glBindTexture(GL_TEXTURE_2D, SpriteSheet.tiles.texture.id);
+		for(int x = 0; x < WIDTH; x += 32)
+		{
+			for(int y = 0; y < HEIGHT; y += 32)
+			{
+				if( y % 64 == 0)
+				{
+					//Renderer.renderTexturedSquare(Sprite.dirt, x, y);
+				} else {
+					//Renderer.renderTexturedSquare(Sprite.cobblestone, x, y);
+				}
+			}
+		}
 	}
 
 	public void update()
